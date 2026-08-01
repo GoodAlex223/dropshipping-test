417beb32f82f2a39e3753d5f5ca2ba67654c8862
GoodAlex223 <alexminak32@gmail.com>
2026-08-01 20:43:31 +0000

    feat(pdp): SizePicker with unit-tested placeholder formula

--- src/components/products/index.ts
+++ src/components/products/index.ts
@@ -2,3 +2,4 @@ export { ProductCard } from "./ProductCard";
 export { QuickViewDialog } from "./QuickViewDialog";
 export type { QuickViewProduct } from "./QuickViewDialog";
 export { SocialShareButtons } from "./SocialShareButtons";
+export { SizePicker } from "./SizePicker";
